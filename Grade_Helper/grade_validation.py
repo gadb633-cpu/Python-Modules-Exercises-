bb40e3afe607e497cb5a190bfceaa61bc17d419d
gad-biton <gadb633@gmail.com>
2026-07-01 16:40:48 +0300

    fixed fu validate_grade

--- Grade_Helper/grade_validation.py
+++ Grade_Helper/grade_validation.py
@@ -1,23 +1,33 @@
 from grade_data import students
 
 def validate_student(student):
-    if isinstance(student,tuple):
-        if len(student) == 2:
-            if isinstance(student[0],str):
-                return True
+    try:
+        if isinstance(student,tuple):
+            if len(student) == 2:
+                if isinstance(student[0],str):
+                    return True
+                else:
+                    return "Skipped student: Name must be a string"
             else:
-                return "Skipped student: Name must be a string"
+                return "Skipped student: tuple must be two"
         else:
-            return "Skipped student: tuple must be two"
-    else:
-        return "Skipped student: is not a tuple"
+            return "Skipped student: is not a tuple"
+    except ValueError:
+        return "valueerror"
+    except TypeError:
+        return "typeerror"
+    
             
                 
 
 def validate_grade(grade):
-    if 0 <grade <= 100:
-        return True
-    else:
-        return "Skipped student: Grade must be between 0 and 100"
-print(validate_grade(120))    
+    try:
+        if 0 <grade <= 100:
+            return True
+        else:
+            return "Skipped student: Grade must be between 0 and 100"
+    except ValueError:
+        return "valueerror"
+    except TypeError:
+        return "typeerror"
 
