966ca08f648904e09413030a6173813f2853e6d9
gad-biton <gadb633@gmail.com>
2026-07-01 16:33:15 +0300

    added fu validate_student

--- Grade_Helper/grade_validation.py
+++ Grade_Helper/grade_validation.py
@@ -1,1 +1,19 @@
-     
+from grade_data import students
+
+def validate_student(student):
+    if isinstance(student,tuple):
+        if len(student) == 2:
+            if isinstance(student[0],str):
+                return True
+            else:
+                return "Skipped student: Name must be a string"
+        else:
+            return "Skipped student: tuple must be two"
+    else:
+        return "Skipped student: is not a tuple"
+            
+                
+
+
+def validate_grade(grade):
+    pass
